6efccc6713e6c7d59ba09dda46f84cfdb2602fb2
bengris32 <bengris32@protonmail.ch>
2022-08-27 22:27:55 +0100

    script: Import scripts
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+C=$ANDROID_BUILD_TOP
+S="private_patches"
+D="frameworks/base"
+
+apply_patches() { cd ${C}/${1}; git apply --ignore-whitespace ${C}/${S}/$1/*.patch; cd ${C}; }
+
+for R in $D; do
+    apply_patches $R
+done
